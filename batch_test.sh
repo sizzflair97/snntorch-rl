008a98217222360cc66bc97091c4e5ea03ed953e
Sizz Flair <sizzflair97@gmail.com>
2023-12-03 15:34:00 +0900

    20231203

--- batch_test.sh
+++ batch_test.sh
@@ -3,9 +3,9 @@ filename="$HOME/myubai/snntorch-rl/cartpole_snn.ipynb"
 
 for ((time_step=1; time_step<=1; time_step+=2));
 do
-    for elg_coeff_prefix in 1 3
+    for elg_coeff_prefix in 1 2.5 4 5.5 7 8.5
     do
-        for elg_coeff_postfix in e-1 e-2 e-3 e-4
+        for elg_coeff_postfix in e-1
         do
             sbatch "sbatch_script.sbatch" "$filename" "$time_step"e-1 "$elg_coeff_prefix$elg_coeff_postfix"
         done
